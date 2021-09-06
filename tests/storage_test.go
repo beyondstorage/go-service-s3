6dbf68fcd3802bc89a36e55a793b147787609fad
Xuanwo <github@xuanwo.io>
2021-09-06 18:24:11 +0800

    test: Add new test case
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- tests/storage_test.go
+++ tests/storage_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"bytes"
 	"os"
 	"testing"
 
@@ -41,4 +42,22 @@ func TestHTTPSigner(t *testing.T) {
 	}
 	tests.TestStorageHTTPSignerWrite(t, setupTest(t))
 	tests.TestStorageHTTPSignerRead(t, setupTest(t))
+}
+
+// https://github.com/beyondstorage/go-storage/issues/741
+func TestIssue741(t *testing.T) {
+	if os.Getenv("STORAGE_S3_INTEGRATION_TEST") != "on" {
+		t.Skipf("STORAGE_S3_INTEGRATION_TEST is not 'on', skipped")
+	}
+	store := setupTest(t)
+
+	content := []byte("Hello, World!")
+	r := bytes.NewReader(content)
+
+	_, err := store.Write("IMG@@@¥&_0960.jpg", r, int64(len(content)))
+	if err != nil {
+		t.Errorf("write: %v", err)
+		return
+	}
+	return
 }
